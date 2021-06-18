4b8a0dbe5c20e9cac4a274c329889c463fc57ee5
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-06-18 18:37:02 -0300

    Geração do campo de reputação com valores aleatórios
    Uso do serializer e da biblioteca random para gerar as reputações das diaristas

--- serializers/city_housekeepers_serializer.py
+++ serializers/city_housekeepers_serializer.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from painel_administrativo.models import Housekeeper
+import random
 
 
 class HousekeeperCitySerializer(serializers.ModelSerializer):
+    reputacao = serializers.SerializerMethodField()
+
     class Meta:
         model = Housekeeper
-        fields = ('nome_completo', 'foto', 'cidade')
+        fields = ('nome_completo', 'foto', 'cidade', 'reputacao')
+
+    def get_reputacao(self, obj):
+        return random.randint(0, 5)
+
